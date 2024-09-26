d2a680ff6ed9af6b883953c21b94ad3f2adc6113
infyf <138444902+infyf@users.noreply.github.com>
2024-09-26 10:51:48 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,16 +2,20 @@ using lr3.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Додаємо сервіси для кожного з контролерів
 builder.Services.AddTransient<CalcService>();
 builder.Services.AddTransient<TimeService>();
 
 
-builder.Services.AddControllers();
+builder.Services.AddControllersWithViews(); 
 
 var app = builder.Build();
 
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapControllers();
+
+app.UseStaticFiles();
 
 app.Run();
